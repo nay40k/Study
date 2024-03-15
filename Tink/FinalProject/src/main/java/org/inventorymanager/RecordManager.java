63871f8466057720b6398ce84f754b8e55001afe
nay40k <nay40k777@gmail.com>
2024-03-15 15:22:24 +0300

    Final project (beginning)

---
+++ Tink/FinalProject/src/main/java/org/inventorymanager/RecordManager.java
@@ -0,0 +1,8 @@
+package org.inventorymanager;
+
+interface RecordManager {
+    void addRecord(OfficeEquipment equipment);
+    void deleteRecord(OfficeEquipment equipment);
+    void editRecord(OfficeEquipment equipment);
+    void viewRecords();
+}
