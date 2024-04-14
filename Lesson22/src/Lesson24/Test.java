9ef03e257a25ba314afd3639c2ad089244a0e017
nay40k <nay40k777@gmail.com>
2024-04-14 23:13:28 +0300

    Lesson24

---
+++ Lesson22/src/Lesson24/Test.java
@@ -0,0 +1,8 @@
+package Lesson24;
+
+public class Test {
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+    }
+}
